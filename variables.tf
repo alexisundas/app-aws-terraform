7519ee8137a8b55b89bb396746002bcf7402f711
Alexis <alexissundas@gmail.com>
2024-05-19 23:53:50 +0300

    changes made try

--- variables.tf
+++ variables.tf
@@ -1,5 +1,13 @@
-variable "aws_access_key_id" {}
-variable "aws_secret_access_key" {}
+variable "aws_access_key_id" {
+  type        = string
+  sensitive   = true
+}
+variable "aws_secret_access_key" {
+  type        = string
+  sensitive   = true
+}
 variable "private_key_path" {
+  type        = string
+  sensitive   = true
   description = "Path to the private key file for SSH access"
 }
