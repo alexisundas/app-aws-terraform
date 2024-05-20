921c57203a9d007cdcaa3068d5c3b289c0ae7df0
Alexis <alexissundas@gmail.com>
2024-05-20 15:18:28 +0300

    Some changes made

--- provider.tf
+++ provider.tf
@@ -1,3 +1,6 @@
 provider "aws" {
-    region  = "eu-central-1"
+    access_key = var.aws_access_key_id
+    secret_key = var.aws_secret_access_key
+    session_token = var.aws_session_token
+    region     = var.aws_region
 }
---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "aws_access_key_id" {
+  description = "AWS access key"
+  type        = string
+}
+
+variable "aws_secret_access_key" {
+  description = "AWS secret access key"
+  type        = string
+}
+
+variable "aws_region" {
+  description = "The AWS region to create resources in"
+  type        = string
+}
+
+variable "aws_session_token" {
+  description = "The AWS Session Token"
+  type        = string
+}
