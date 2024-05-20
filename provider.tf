592d47aa8d2949d7960678a35a79151328118ff5
Alexis <alexissundas@gmail.com>
2024-05-20 20:39:27 +0300

    github action only on tag

--- provider.tf
+++ provider.tf
@@ -1,3 +1,18 @@
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "5.16.1"
+        }
+    }
+
+    backend "s3" {
+        bucket = "terraform-remote-backend-s3"
+        key    = "dev/terraform.tfstate"
+        region = "eu-central-1"
+    }
+}
+
 provider "aws" {
     access_key = var.aws_access_key_id
     secret_key = var.aws_secret_access_key
