779dc91b7e9eae4055f744c3f4b0b3c9e9475ad6
Alexis <alexissundas@gmail.com>
2024-05-21 23:17:11 +0300

    Here we go again

--- modules/ec2/outputs.tf
+++ modules/ec2/outputs.tf
@@ -2,10 +2,3 @@ output "instances" {
   value = aws_instance.web.*.id
 }
 
-output "public_ip" {
-  value = aws_instance.web.*.public_ip
-}
-
-output "private_key_content" {
-  value = aws_key_pair.deployer.key_name
-}
